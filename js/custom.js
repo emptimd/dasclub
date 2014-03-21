2e7e3bd1940c5b221c20035107435d5ad8c390f1
Bogdan Varticovschi <emptimd@gmail.com>
2014-03-21 08:03:33 +0200

    first commit

---
+++ js/custom.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+    $('.slider1').bxSlider({
+        minSlides: 3,
+        moveSlides:1,
+        maxSlides: 3,
+        slideMargin: 11,
+        slideWidth:326,
+        infiniteLoop:false,
+        pager:false,
+        nextText:'',
+        prevText:''
+    });
+
+    $('.slider-main').bxSlider({
+        minSlides: 1,
+        moveSlides:1,
+        maxSlides: 1,
+        slideMargin: 0,
+        slideWidth:690,
+        infiniteLoop:false,
+        pager:true,
+        nextText:'',
+        prevText:''
+    });
+});
